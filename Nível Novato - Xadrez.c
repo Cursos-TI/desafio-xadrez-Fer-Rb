16119967b2bc964c24a3696fd70740057eca3f75
Fer-Rb <fernandaazevedo2682@gmail.com>
2025-10-24 22:10:16 +0000

    Nível Aventureiro concluído

--- Nível Novato - Xadrez.c
+++ Nível Novato - Xadrez.c
@@ -5,7 +5,7 @@ int main()
 
     int horBispo = 1, vertBispo = 1, movRainha = 1, movTorre;
 
-    // A primeira estrutura de repetição será o for para a peça Torre, essa peça tem que mover 5 vezes para a direita.
+    // A o estrutura de repetição será o for para a peça Torre, essa peça tem que mover 5 vezes para a direita.
 
     printf("Peça: Torre\n");
     for (movTorre = 1; movTorre <= 5; movTorre++)
@@ -15,8 +15,8 @@ int main()
     // Espaço usado somente para estética do código.
     printf("\n\n");
 
-    // Próxima peça é o Bispo com o uso do while para os movimentos, o Bispo deverá se mover 5 vez na diagonal para cima e direita.
-    // Os eixos são usados para o usuário saber que a peça está se movendo diagonalmente, mas ainda fazendo o movimento simples de ir pra cima e pra a direita.
+    // o peça é o Bispo com o uso do while para os movimentos, o Bispo deverá se mover 5 vez na diagonal para cima e direita.
+    // Os eixos são usados para o usuário saber que a peça está se movendo diagonalmente, mas ainda fazendo o movimento simples de ir o cima e o a direita.
 
     printf("Peça: Bispo\n");
     while (horBispo <= 5 && vertBispo <= 5)
